8e1c0d96dda143e5326d6534351fa5345b006bc0
Dalafiari Samuel <dalafiarisamuel@gmail.com>
2026-01-06 08:50:22 +0100

    feat: Add local database and repository modules
    This commit introduces the necessary Koin modules for setting up a local database using SQLite and managing local image data.
    
    Key changes include:
    - Added a `sharedModule` to provide the database instance and PhotoDao.
    - Configured `BundledSQLiteDriver` and `Dispatchers.IO` for the database.
    - Integrated `UnsplashImageLocalRepository` and its implementation into the repository module.

--- composeApp/src/commonMain/kotlin/di/Koin.kt
+++ composeApp/src/commonMain/kotlin/di/Koin.kt
@@ -1,12 +1,17 @@
 package di
 
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
+import data.local.database.DatabaseFactory
+import data.local.database.UnsplashPhotoDatabase
+import data.local.repository.UnsplashImageLocalRepository
 import data.mapper.PhotoCreatorMapper
 import data.mapper.PhotoMapper
 import data.mapper.PhotosUrlsMapper
-import data.repository.ImageRepository
+import data.remote.repository.ImageRepository
 import data.repository.ImageRepositoryImpl
-import data.repository.SharedRepository
 import data.repository.SharedRepositoryImpl
+import data.repository.UnsplashImageLocalRepositoryImpl
+import data.ui.repository.SharedRepository
 import de.jensklingenberg.ktorfit.Ktorfit
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
@@ -15,13 +20,17 @@ import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlinx.serialization.json.Json
 import networking.ApiInterface
 import networking.AuthorizationTokenInterceptor
 import networking.createApiInterface
 import org.koin.core.context.startKoin
+import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.KoinAppDeclaration
+import org.koin.dsl.bind
 import org.koin.dsl.module
 import ui.viewmodel.HomeScreenViewModel
 import ui.viewmodel.PhotoDetailViewModel
@@ -34,7 +43,8 @@ fun initKoin(appDeclaration: KoinAppDeclaration = {}) =
             mapperModule(),
             repositoryModule(),
             viewModelModule(),
-            platformModule()
+            platformModule(),
+            sharedModule()
         )
     }
 
@@ -83,9 +93,22 @@ private fun mapperModule() = module {
 private fun repositoryModule() = module {
     single<ImageRepository> { ImageRepositoryImpl(get()) }
     single<SharedRepository> { SharedRepositoryImpl() }
+    singleOf(::UnsplashImageLocalRepositoryImpl).bind<UnsplashImageLocalRepository>()
 }
 
 private fun viewModelModule() = module {
     viewModelOf(::HomeScreenViewModel)
     viewModelOf(::PhotoDetailViewModel)
+}
+
+private fun sharedModule() = module {
+    single {
+        get<UnsplashPhotoDatabase>().getPhotoDao()
+    }
+    single {
+        get<DatabaseFactory>().create()
+            .setDriver(BundledSQLiteDriver())
+            .setQueryCoroutineContext(Dispatchers.IO)
+            .build()
+    }
 }
